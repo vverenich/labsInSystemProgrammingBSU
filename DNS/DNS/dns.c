78ad39662a6e53b09b8150e1d53ce5a423610f26
Влад Веренич <vverchickktv@gmail.com>
2021-09-22 14:02:07 +0300

    change dns.c

--- DNS/DNS/dns.c
+++ DNS/DNS/dns.c
@@ -10,114 +10,120 @@
 #include <stdio.h>
 #include "dns.h"
 
-typedef struct _Pair
+struct Pair
 {
-	char *domen;
 	IPADDRESS ip;
-	struct _Pair *next_pair;
-}Pair;
+	char* domen;
+	struct Pair* nextPair;
+};
 
-const int SIZE = 12837;
+const int SIZE = 12838;
 
 
 DNSHandle InitDNS()
 {
-	DNSHandle hDNS = (unsigned int)(Pair *)calloc(SIZE, sizeof(Pair));
-	if ((Pair *)hDNS == NULL) {
-		return INVALID_DNS_HANDLE;
+	DNSHandle hDNS = (unsigned int)(Pair*)calloc(SIZE, sizeof(Pair));
+	if ((Pair*)hDNS != NULL)
+	{
+		return hDNS;
 	}
-	return hDNS;
+	return INVALID_DNS_HANDLE;
 }
 
-unsigned int HashRs(const char *str)//Хеш-функция
+/*
+* hash function
+*/
+unsigned int HashFunc(const char* str)
 {
-
 	unsigned int hash = 0;
 
-	for (; *str; str++)
+	while (c = *str++)
 	{
-		hash += (unsigned char)(*str);
-		hash += (hash << 10);
-		hash ^= (hash >> 6);
+		hash = ((hash << 5) + hash) + c;
 	}
-	hash += (hash << 3);
-	hash ^= (hash >> 11);
-	hash += (hash << 15);
 
 	return hash % SIZE;
 
 }
-void Add_to_table(DNSHandle hDNS, char *str, IPADDRESS ip) { //Добавление элемента в хеш-таблицу
-	Pair *p;
-	unsigned int uHostNameLength = strlen(str);
-	unsigned int index_element = HashRs(str);
-	if (((Pair *)hDNS)[index_element].domen == NULL)
-	{
-		((Pair *)hDNS)[index_element].domen = (char *)malloc(uHostNameLength + 1);
-		strcpy(((Pair *)hDNS)[index_element].domen, str);
-		((Pair *)hDNS)[index_element].ip = ip;
 
-	} else
+/*
+* Function that adds element to hash table
+*/
+void AddToTable(DNSHandle hDNS, char* str, IPADDRESS ip)
+{
+	unsigned int lengthOfHost = strlen(str);
+	unsigned int indexOfElement = HashFunc(str);
+
+	if (((Pair*)hDNS)[indexOfElement].domen == NULL)
 	{
-		p = (Pair *)malloc(sizeof(Pair));
-		p->domen = (char *)malloc(uHostNameLength + 1);
-		strcpy(p->domen, str);
+		((Pair*)hDNS)[indexOfElement].ip = ip;
+		((Pair*)hDNS)[indexOfElement].domen = (char*)malloc(lengthOfHost + 1);
+		strcpy(((Pair*)hDNS)[indexOfElement].domen, str);
+	}
+	else
+	{
+		Pair* p;
+		p = (Pair*)malloc(sizeof(Pair));
 		p->ip = ip;
-		p->next_pair = ((Pair *)hDNS)[index_element].next_pair;
-		((Pair *)hDNS)[index_element].next_pair = p;
+		p->domen = (char*)malloc(lengthOfHost + 1);
+		strcpy(p->domen, str);
+		p->nextPair = ((Pair*)hDNS)[indexOfElement].nextPair;
+		((Pair*)hDNS)[indexOfElement].nextPair = p;
 	}
 
 }
-void LoadHostsFile(DNSHandle hDNS, const char *hostsFilePath)
+void LoadHostsFile(DNSHandle hDNS, const char* hostsFilePath)
 {
-	FILE *fInput = NULL;
+	FILE* fInput = NULL;
 
 	fInput = fopen(hostsFilePath, "r");
 
 	unsigned int ip1 = 0, ip2 = 0, ip3 = 0, ip4 = 0;
-	char *string = (char *)malloc(201);
+	char* string = (char*)malloc(201);
 
 	if (NULL == fInput)
 		return;
 	while (fscanf_s(fInput, "%d.%d.%d.%d %s", &ip1, &ip2, &ip3, &ip4, string, 200) != EOF)
 	{
-		IPADDRESS ip = (ip1 & 0xFF) << 24 |
-			(ip2 & 0xFF) << 16 |
-			(ip3 & 0xFF) << 8 |
-			(ip4 & 0xFF);
-		Add_to_table(hDNS, string, ip);
+		IPADDRESS ip = (ip1 & 0xFF) << 24 | (ip2 & 0xFF) << 16 | (ip3 & 0xFF) << 8 | (ip4 & 0xFF);
+		AddToTable(hDNS, string, ip);
 	}
 	free(string);
 	fclose(fInput);
 }
 
-IPADDRESS DnsLookUp(DNSHandle hDNS, const char *hostName)
+IPADDRESS DnsLookUp(DNSHandle hDNS, const char* hostName)
 {
-	unsigned int index = HashRs(hostName);
-	Pair *it = &((Pair *)hDNS)[index];
+	unsigned int index = HashFunc(hostName);
+	Pair* it = &((Pair*)hDNS)[index];
 	while (it != NULL && strcmp(it->domen, hostName) != 0)
 	{
-		it = it->next_pair;
+		it = it->nextPair;
 	}
 	if (it != NULL)
+	{
 		return it->ip;
+	}
 	return INVALID_IP_ADDRESS;
 }
 
 void ShutdownDNS(DNSHandle hDNS)
 {
-	Pair *p;
-	Pair *q;
-	for (int i = 0; i < SIZE; i++) {
-		q = &(((Pair *)hDNS)[i]);
+	Pair* p;
+	Pair* q;
+	for (int i = 0; i < SIZE; i++)
+	{
+		q = &(((Pair*)hDNS)[i]);
 		while (q != NULL)
 		{
 			p = q;
-			q = q->next_pair;
+			q = q->nextPair;
 			free(p->domen);
 			free(p);
 		}
 	}
-	if ((Pair *)hDNS != NULL)
-		free((Pair *)hDNS);
+	if ((Pair*)hDNS != NULL)
+	{
+		free((Pair*)hDNS);
+	}
 }
