8cab80cda23df40edc8013d3e20d663da85fab4d
Влад Веренич <vverchickktv@gmail.com>
2021-10-02 17:00:15 +0300

    add all the files

---
+++ lab_2_FW/lab_2_FW/splpv1.c
@@ -0,0 +1,91 @@
+/*
+ * SPLPv1.c
+ * The file is part of practical task for System programming course.
+ * This file contains validation of SPLPv1 protocol.
+ */
+
+
+#error Specify your name and group
+ /*
+   Фамилия, Имя, Отчество
+   № группы
+ */
+
+
+
+ /*
+ ---------------------------------------------------------------------------------------------------------------------------
+ # |      STATE      |         DESCRIPTION       |           ALLOWED MESSAGES            | NEW STATE | EXAMPLE
+ --+-----------------+---------------------------+---------------------------------------+-----------+----------------------
+ 1 | INIT            | initial state             | A->B     CONNECT                      |     2     |
+ --+-----------------+---------------------------+---------------------------------------+-----------+----------------------
+ 2 | CONNECTING      | client is waiting for con-| A<-B     CONNECT_OK                   |     3     |
+   |                 | nection approval from srv |                                       |           |
+ --+-----------------+---------------------------+---------------------------------------+-----------+----------------------
+ 3 | CONNECTED       | Connection is established | A->B     GET_VER                      |     4     |
+   |                 |                           |        -------------------------------+-----------+----------------------
+   |                 |                           |          One of the following:        |     5     |
+   |                 |                           |          - GET_DATA                   |           |
+   |                 |                           |          - GET_FILE                   |           |
+   |                 |                           |          - GET_COMMAND                |           |
+   |                 |                           |        -------------------------------+-----------+----------------------
+   |                 |                           |          GET_B64                      |     6     |
+   |                 |                           |        ------------------------------------------------------------------
+   |                 |                           |          DISCONNECT                   |     7     |
+ --+-----------------+---------------------------+---------------------------------------+-----------+----------------------
+ 4 | WAITING_VER     | Client is waiting for     | A<-B     VERSION ver                  |     3     | VERSION 2
+   |                 | server to provide version |          Where ver is an integer (>0) |           |
+   |                 | information               |          value. Only a single space   |           |
+   |                 |                           |          is allowed in the message    |           |
+ --+-----------------+---------------------------+---------------------------------------+-----------+----------------------
+ 5 | WAITING_DATA    | Client is waiting for a   | A<-B     CMD data CMD                 |     3     | GET_DATA a GET_DATA
+   |                 | response from server      |                                       |           |
+   |                 |                           |          CMD - command sent by the    |           |
+   |                 |                           |           client in previous message  |           |
+   |                 |                           |          data - string which contains |           |
+   |                 |                           |           the following allowed cha-  |           |
+   |                 |                           |           racters: small latin letter,|           |
+   |                 |                           |           digits and '.'              |           |
+ --+-----------------+---------------------------+---------------------------------------+-----------+----------------------
+ 6 | WAITING_B64_DATA| Client is waiting for a   | A<-B     B64: data                    |     3     | B64: SGVsbG8=
+   |                 | response from server.     |          where data is a base64 string|           |
+   |                 |                           |          only 1 space is allowed      |           |
+ --+-----------------+---------------------------+---------------------------------------+-----------+----------------------
+ 7 | DISCONNECTING   | Client is waiting for     | A<-B     DISCONNECT_OK                |     1     |
+   |                 | server to close the       |                                       |           |
+   |                 | connection                |                                       |           |
+ ---------------------------------------------------------------------------------------------------------------------------
+
+ IN CASE OF INVALID MESSAGE THE STATE SHOULD BE RESET TO 1 (INIT)
+
+ */
+
+
+#include "splpv1.h"
+
+
+
+
+
+ /* FUNCTION:  validate_message
+  *
+  * PURPOSE:
+  *    This function is called for each SPLPv1 message between client
+  *    and server
+  *
+  * PARAMETERS:
+  *    msg - pointer to a structure which stores information about
+  *    message
+  *
+  * RETURN VALUE:
+  *    MESSAGE_VALID if the message is correct
+  *    MESSAGE_INVALID if the message is incorrect or out of protocol
+  *    state
+  */
+enum test_status validate_message(struct Message* msg)
+{
+
+    // TODO: Implement me
+
+    return MESSAGE_VALID;
+}
